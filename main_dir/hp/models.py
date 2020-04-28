e5f40cb0367636e4cc793bed7a6a62cd02197f30
mobronfin <59948664+mobronfin@users.noreply.github.com>
2020-04-28 00:46:58 -0400

    Scraper function and headline extractor class

--- main_dir/hp/models.py
+++ main_dir/hp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+#class to extrat headline of news articles
+class Extract_Headline(models.Model):
+  title = models.CharField(max_length=300)
+  # image = models.URLField(null=True, blank=True)
+  url = models.TextField()
+
+  def __str__(self):
+    return self.title
